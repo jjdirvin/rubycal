86cd2559b9efbae758bece92433737f6076e33b5
Joshua <jdirvin@unomaha.edu>
2017-06-13 21:03:23 +0000

    please work

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,7 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      # YOUR CODE HERE HI
     }
   end
 end
